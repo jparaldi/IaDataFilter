0f787da6b8cc3a751ffead237946e7ed68318b1c
araldi <jpedro.araldi@gmail.com>
2025-07-10 09:27:27 -0300

    verificação básica de licenca de uso

--- main_code/basic_crawl.py
+++ main_code/basic_crawl.py
@@ -1,9 +1,9 @@
 import asyncio
-from crawl4ai import AsyncWebCrawler
+from crawl4ai import AsyncWebCrawler # type: ignore
 
 async def main():
     async with AsyncWebCrawler() as crawler:
-        result = await crawler.arun("https://dados.gov.br/dados/conjuntos-dados")
+        result = await crawler.arun("https://dados.gov.br/dados/conjuntos-dados/patrimonio-genetico-nacional-que-foram-envolvidas-em-pesquisas-realizadas-na-ufam-por-ano")
         print(result.markdown)
 
 asyncio.run(main())
