02ece8bec0fdcea293d8b4b5efb9843a5805ef13
João Araldi <168310220+jparaldi@users.noreply.github.com>
2025-08-08 12:45:17 -0300

    Delete main_code directory

--- main_code/basic_crawl.py
+++
@@ -1,9 +0,0 @@
-import asyncio
-from crawl4ai import AsyncWebCrawler # type: ignore
-
-async def main():
-    async with AsyncWebCrawler() as crawler:
-        result = await crawler.arun("https://dados.gov.br/dados/conjuntos-dados/patrimonio-genetico-nacional-que-foram-envolvidas-em-pesquisas-realizadas-na-ufam-por-ano")
-        print(result.markdown)
-
-asyncio.run(main())
